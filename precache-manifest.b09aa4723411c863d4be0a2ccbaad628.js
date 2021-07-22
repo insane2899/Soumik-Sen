37910a7c4dada598e327036846daf5cc356b7619
insane2899 <imrimosen@gmail.com>
2021-07-22 21:14:34 +0530

    Updates

---
+++ precache-manifest.b09aa4723411c863d4be0a2ccbaad628.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a06e65e62141811aa06d374266cd5e",
+    "url": "/Soumik-Sen/index.html"
+  },
+  {
+    "revision": "6a891ab3ba0a911d8bf7",
+    "url": "/Soumik-Sen/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "9677ab66112d549b9e61",
+    "url": "/Soumik-Sen/static/css/main.aed7dc35.chunk.css"
+  },
+  {
+    "revision": "6a891ab3ba0a911d8bf7",
+    "url": "/Soumik-Sen/static/js/2.9e22ed80.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/Soumik-Sen/static/js/2.9e22ed80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9677ab66112d549b9e61",
+    "url": "/Soumik-Sen/static/js/main.458ab818.chunk.js"
+  },
+  {
+    "revision": "1a2920eb19eb74ed9fbf",
+    "url": "/Soumik-Sen/static/js/runtime-main.24c1e5d2.js"
+  },
+  {
+    "revision": "82e7ae12e2baeff29c9ab4dfdfa4ee91",
+    "url": "/Soumik-Sen/static/media/Soumik.82e7ae12.jpeg"
+  },
+  {
+    "revision": "0602a72d2d617f75dbcd1dfceace3c74",
+    "url": "/Soumik-Sen/static/media/desktop2.0602a72d.jpg"
+  },
+  {
+    "revision": "218443b1ec1e25d7297a93a34ad77021",
+    "url": "/Soumik-Sen/static/media/forest2.218443b1.png"
+  },
+  {
+    "revision": "32d8645ebf2dfb8e479066de7ca85fe6",
+    "url": "/Soumik-Sen/static/media/lion.32d8645e.jpg"
+  },
+  {
+    "revision": "5fa2b94e2451bbb1090879ba8b2abc84",
+    "url": "/Soumik-Sen/static/media/soumik.5fa2b94e.jpg"
+  },
+  {
+    "revision": "690f02a22adf21dad4cd92eb8f351e32",
+    "url": "/Soumik-Sen/static/media/stairs.690f02a2.jpeg"
+  },
+  {
+    "revision": "dd8c2d8f3dc7fa0e602117dbbe4000e9",
+    "url": "/Soumik-Sen/static/media/test.dd8c2d8f.jpg"
+  }
+]);
